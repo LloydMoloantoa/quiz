6b3b2cc9d7775bbf0a3a0dd51c4bd5a23be3f414
Lloyd Moloantoa <56023929+LloydMoloantoa@users.noreply.github.com>
2022-10-23 19:32:08 +0200

    last

--- game/end/js/end.js
+++ game/end/js/end.js
@@ -1,29 +1,25 @@
-const username = document.getElementById('username');
-const saveScoreBtn = document.getElementById('saveScoreBtn');
+const career = document.getElementById('career');
+const resultBtn = document.querySelector('.resultBtn');
 const finalScore = document.getElementById('finalScore');
-const mostRecentScore = localStorage.getItem('mostRecentScore');
+var mostRecentScore = localStorage.getItem('mostRecentScore');
+var careerLbl = document.querySelector('.careerLbl');
 
-const highScores = JSON.parse(localStorage.getItem('highScores')) || [];
+let counter = 0;
 
-const MAX_HIGH_SCORES = 5;
 
-finalScore.innerText = mostRecentScore;
+resultBtn.addEventListener('click', function () {
+    finalScore.innerText = mostRecentScore;
 
-username.addEventListener('keyup', () => {
-    saveScoreBtn.disabled = !username.value;
+    if (parseInt(mostRecentScore) < 30) {
+        careerLbl.innerHTML = "According to your results, you would be suited for this Cloud. Go to the link for more info: https://vargelloyd.azurewebsites.net/lessons/14";
+    }
+    else if (parseInt(mostRecentScore) >= 30 && parseInt(mostRecentScore) < 50) {
+       
+        careerLbl.innerHTML = "According to your results, you would be suited for this AWS. Go to the link for more info: https://vargelloyd.azurewebsites.net/lessons/24";
+    }
+    else if (parseInt(mostRecentScore) >= 50 && parseInt(mostRecentScore) < 100) {
+       
+        careerLbl.innerHTML = "According to your results, you would be suited for this Full Stact. Go to the link for more info: https://vargelloyd.azurewebsites.net/lessons/4";
+    }
+
 });
-
-saveHighScore = (e) => {
-    e.preventDefault();
-
-    const score = {
-        score: mostRecentScore,
-        name: username.value,
-    };
-    highScores.push(score);
-    highScores.sort((a, b) => b.score - a.score);
-    highScores.splice(5);
-
-    localStorage.setItem('highScores', JSON.stringify(highScores));
-    window.location.assign('/');
-};
